aa612763edaa4d3d12ed2840e98457c50912e24e
limeng32 <limeng32@live.cn>
2017-09-17 21:24:20 +0800

    增加controller

---
+++ src/main/java/indi/demo/flying/web/CommonController.java
@@ -0,0 +1,134 @@
+package indi.demo.flying.web;
+
+import java.util.Calendar;
+import java.util.Collection;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import indi.demo.flying.entity.Cart;
+import indi.demo.flying.entity.CartCommodity;
+import indi.demo.flying.entity.Commodity;
+import indi.demo.flying.service.CartCommodityService;
+import indi.demo.flying.service.CartService;
+import indi.demo.flying.service.CommodityService;
+import indi.mybatis.flying.models.Conditionable;
+import indi.mybatis.flying.pagination.Order;
+import indi.mybatis.flying.pagination.Page;
+import indi.mybatis.flying.pagination.PageParam;
+import indi.mybatis.flying.pagination.SortParam;
+
+@Controller
+public class CommonController {
+
+	public static final String UNIQUE_VIEW_NAME = "__unique_view_name";
+
+	@Autowired
+	private CartService cartService;
+
+	@Autowired
+	private CartCommodityService cartCommodityService;
+
+	@Autowired
+	private CommodityService commodityService;
+
+	@RequestMapping(method = { RequestMethod.GET }, value = "/getCart")
+	public String getCart(HttpServletRequest request, HttpServletResponse response, ModelMap mm,
+			@RequestParam("id") String id) {
+		Cart cart = cartService.mySelect(id);
+		mm.addAttribute("_content", cart);
+		return UNIQUE_VIEW_NAME;
+	}
+
+	@RequestMapping(method = { RequestMethod.GET }, value = "/getCommodity")
+	public String getCommodity(HttpServletRequest request, HttpServletResponse response, ModelMap mm,
+			@RequestParam("id") String id) {
+		Commodity commodity = commodityService.mySelect(id);
+		mm.addAttribute("_content", commodity);
+		return UNIQUE_VIEW_NAME;
+	}
+
+	@RequestMapping(method = { RequestMethod.GET }, value = "/addCommodity")
+	public String AddCommodity(HttpServletRequest request, HttpServletResponse response, ModelMap mm,
+			@RequestParam(value = "price", required = false) Integer price,
+			@RequestParam(value = "name", required = false) String name) {
+		Commodity commodity = new Commodity();
+		commodity.setName(name);
+		commodity.setPrice(price);
+		commodityService.myInsert(commodity);
+		mm.addAttribute("_content", commodity);
+		return UNIQUE_VIEW_NAME;
+	}
+
+	@RequestMapping(method = { RequestMethod.GET }, value = "/updateCommodity")
+	public String updateCommodity(HttpServletRequest request, HttpServletResponse response, ModelMap mm,
+			@RequestParam(value = "id", required = false) Integer id,
+			@RequestParam(value = "price", required = false) Integer price,
+			@RequestParam(value = "name", required = false) String name) {
+		Commodity commodity = commodityService.mySelect(id);
+		if (commodity != null) {
+			if (name != null) {
+				commodity.setName(name);
+			}
+			if (price != null) {
+				commodity.setPrice(price);
+			}
+			commodityService.myUpdate(commodity);
+		}
+		mm.addAttribute("_content", commodity);
+		return UNIQUE_VIEW_NAME;
+	}
+
+	@RequestMapping(method = { RequestMethod.GET }, value = "/getCommodityByCart")
+	public String getCommodityByCart(HttpServletRequest request, HttpServletResponse response, ModelMap mm,
+			@RequestParam(value = "id", required = false) String cartId) {
+		Cart cartCondition = new Cart();
+		cartCondition.setId(cartId);
+		CartCommodity cartCommodityCondition = new CartCommodity();
+		cartCommodityCondition.setCart(cartCondition);
+		Collection<CartCommodity> cartCommodityC = cartCommodityService.mySelectAll(cartCommodityCondition);
+		mm.addAttribute("_content", cartCommodityC);
+		return UNIQUE_VIEW_NAME;
+	}
+
+	@RequestMapping(method = { RequestMethod.GET }, value = "/dealCart")
+	public String dealCart(HttpServletRequest request, HttpServletResponse response, ModelMap mm,
+			@RequestParam(value = "id", required = false) String cartId) {
+		Cart cart = cartService.mySelect(cartId);
+		if (cart != null) {
+			cart.setDeal(true);
+			cart.setDealTime(Calendar.getInstance().getTime());
+			cartService.myUpdate(cart);
+
+			CartCommodity cartCommodityCondition = new CartCommodity();
+			cartCommodityCondition.setCart(cart);
+			Collection<CartCommodity> cartCommodityC = cartCommodityService.mySelectAll(cartCommodityCondition);
+			mm.addAttribute("_content", cartCommodityC);
+		}
+		return UNIQUE_VIEW_NAME;
+	}
+
+	@RequestMapping(method = { RequestMethod.GET }, value = "/undealCart")
+	public String unDealCart(HttpServletRequest request, HttpServletResponse response, ModelMap mm,
+			@RequestParam(value = "id", required = false) String cartId) {
+		Cart cart = cartService.mySelect(cartId);
+		if (cart != null) {
+			cart.setDeal(false);
+			cart.setDealTime(null);
+			cartService.myUpdatePersistent(cart);
+
+			CartCommodity cartCommodityCondition = new CartCommodity();
+			cartCommodityCondition.setCart(cart);
+			Collection<CartCommodity> cartCommodityC = cartCommodityService.mySelectAll(cartCommodityCondition);
+			mm.addAttribute("_content", cartCommodityC);
+		}
+		return UNIQUE_VIEW_NAME;
+	}
+}
--- src/main/resources/INIT_TABLE2.sql
+++ src/main/resources/INIT_TABLE2.sql
@@ -1,96 +1,37 @@
-drop table if exists CART;
+drop table if exists PERSON;
 
-drop table if exists Commodity;
+drop table if exists ROLE;
 
-drop table if exists CART_COMMODITY;
-
-create table CART
+create table ROLE
 (
    ID         			varchar(40) not null ,
-   DEAL                 tinyint ,
-   DEAL_TIME            datetime ,
+   NAME                 varchar(40) ,
+   VALUE            	varchar(20) ,
    primary key (ID)
 );
 
-insert into CART
-values ('1', '0', null);
+insert into ROLE
+values ('1', '普通会员', 'normal');
 
-insert into CART
-values ('2', '0', null);
+insert into ROLE
+values ('2', '银牌会员', 'silver');
 
-create table COMMODITY
+insert into ROLE
+values ('3', '金牌会员', 'gold');
+
+create table PERSON
 (
    ID         			varchar(40) not null ,
-   NAME                 varchar(64) ,
-   PRICE            	integer ,
+   NAME                 varchar(40) ,
+   ROLE_ID            	varchar(40) ,
    primary key (ID)
 );
 
-insert into COMMODITY
-values ('1', '牙刷A', '1200');
+insert into PERSON
+values ('1', '张三', '1');
 
-insert into COMMODITY
-values ('2', '牙刷B', '1850');
+insert into PERSON
+values ('2', '李四', '2');
 
-insert into COMMODITY
-values ('3', '牙刷C', '2100');
-
-insert into COMMODITY
-values ('4', '佳洁士牙膏', '1499');
-
-insert into COMMODITY
-values ('5', '六必治牙膏', '1999');
-
-insert into COMMODITY
-values ('6', '云南白药牙膏', '2499');
-
-insert into COMMODITY
-values ('7', '潘婷洗发露', '3500');
-
-insert into COMMODITY
-values ('8', '多芬洗发露', '3900');
-
-insert into COMMODITY
-values ('9', '海飞丝洗发露', '5100');
-
-insert into COMMODITY
-values ('10', '浴液-1500ML', '2800');
-
-insert into COMMODITY
-values ('11', '浴液-2000ML', '3200');
-
-insert into COMMODITY
-values ('12', '浴液-4000ML', '4900');
-
-create table CART_COMMODITY
-(
-   ID         			varchar(40) not null ,
-   CART_ID              varchar(40) ,
-   COMM_ID				varchar(40) ,
-   AMOUNT            	integer ,
-   primary key (ID)
-);
-
-insert into CART_COMMODITY
-values ('1', '1', '1', '3');
-
-insert into CART_COMMODITY
-values ('2', '1', '5', '4');
-
-insert into CART_COMMODITY
-values ('3', '1', '8', '1');
-
-insert into CART_COMMODITY
-values ('4', '1', '12', '1');
-
-insert into CART_COMMODITY
-values ('5', '2', '2', '2');
-
-insert into CART_COMMODITY
-values ('6', '2', '4', '1');
-
-insert into CART_COMMODITY
-values ('7', '2', '9', '2');
-
-insert into CART_COMMODITY
-values ('8', '2', '11', '1');
+insert into PERSON
+values ('3', '王五', '3');
