f86da1500d3fb73199bf74e6dbe56248c99c3edf
limeng32 <limeng32@live.cn>
2017-10-28 11:17:14 +0800

    更新flying依赖到0.9.2

--- src/main/java/indi/demo/flying/entity/Cart.java
+++ src/main/java/indi/demo/flying/entity/Cart.java
@@ -2,15 +2,18 @@ package indi.demo.flying.entity;
 
 import java.io.Serializable;
 
+import javax.persistence.Column;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
 import org.apache.ibatis.type.JdbcType;
 
 import com.alibaba.fastjson.annotation.JSONField;
 
 import indi.demo.flying.entity2.Person;
 import indi.mybatis.flying.annotations.FieldMapperAnnotation;
-import indi.mybatis.flying.annotations.TableMapperAnnotation;
 
-@TableMapperAnnotation(tableName = "CART")
+@Table(name = "CART")
 public class Cart implements Serializable {
 
 	private static final long serialVersionUID = 1L;
@@ -19,24 +22,29 @@ public class Cart implements Serializable {
 	 * 主键，以UUID方式保存
 	 * 
 	 */
-	@FieldMapperAnnotation(dbFieldName = "ID", jdbcType = JdbcType.VARCHAR, isUniqueKey = true)
+	@Id
+	@Column(name = "ID")
 	private String id;
 
 	/**
 	 * deal success?
 	 * 
 	 */
-	@FieldMapperAnnotation(dbFieldName = "DEAL", jdbcType = JdbcType.BOOLEAN)
+	@Column(name = "DEAL")
 	private Boolean deal;
 
 	/**
 	 * 交易时间
 	 * 
 	 */
-	@FieldMapperAnnotation(dbFieldName = "DEAL_TIME", jdbcType = JdbcType.TIMESTAMP)
+	@Column(name = "DEAL_TIME")
 	@JSONField(format = "yyyy-MM-dd hh:mm:ss")
 	private java.util.Date dealTime;
 
+	/**
+	 * 跨数据源关联 person
+	 * 
+	 */
 	@FieldMapperAnnotation(dbFieldName = "PERSON_ID", jdbcType = JdbcType.VARCHAR, dbAssociationTypeHandler = indi.demo.flying.typeHandler.PersonTypeHandler.class)
 	private Person person;
 
